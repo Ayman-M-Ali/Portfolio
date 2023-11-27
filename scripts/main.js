f27e73fc75f70af6b307ed110d90ba4a43ba264f
Ayman Mohammad Ali <ayman.mohmd.ali@gmail.com>
2023-11-27 22:52:16 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,20 @@ bioPic.style.width = '26rem';
 //---
 // Add Skill Underline
 /* eslint-disable */
-const skillsList = document.querySelectorAll('.skills__list');
-const cbFunction = (skillItem) => {
-  const activeElement = document.querySelector('.underline');
-
-    if (activeElement) {
-      activeElement.classList.remove('underline');
-    }
-
-    skillItem.classList.add('underline');
-  };
-
-skillsList.forEach((skillItem) => {
-  skillItem.addEventListener('click', () => cbFunction(skillItem));
-});
+//const skillsList = document.querySelectorAll('.skills__list');
+//const cbFunction = (skillItem) => {
+//  const activeElement = document.querySelector('.underline');
+//
+//    if (activeElement) {
+//      activeElement.classList.remove('underline');
+//    }
+//
+//    skillItem.classList.add('underline');
+//  };
+//
+//skillsList.forEach((skillItem) => {
+//  skillItem.addEventListener('click', () => cbFunction(skillItem));
+//});
 
 // ObserveInterSections API 
 const sectionElements = document.querySelectorAll('section');
