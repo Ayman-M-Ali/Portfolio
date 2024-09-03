b16656fd17d7d714f40bbdbcebb67d2659f59176
Ayman-M-Ali <ayman.mohmd.ali@gmail.com>
2024-09-04 00:40:04 +0300

    Adding LocalStorage property for Theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,28 @@
 import { observeElements } from './utilis.js';
 
+
+// Save Theme on Local Storage
+const toggle = document.querySelector('.toggle__checkbox');
+
+const defaultTheme = '#d4a736';
+const alternativeTheme = '#ef4f67';
+
+// Apply initial theme on page load
+const initialTheme = localStorage.getItem('theme') || defaultTheme; // Default theme
+document.documentElement.style.setProperty('--theme-color', initialTheme);
+toggle.checked = initialTheme === alternativeTheme; // Set checkbox state based on the saved theme
+
+toggle.addEventListener('change', (e) => {
+  const theme = e.target.checked ? alternativeTheme : defaultTheme; // Concise theme assignment based on checkbox state
+  localStorage.setItem('theme', theme);
+  document.documentElement.style.setProperty('--theme-color', theme); // Apply the theme
+});
+
+// CSS should use the variable for theming
+document.documentElement.style.setProperty('--theme-color', initialTheme);
+
+//=========================================
+
 // Resize Pic Pio
 const bioPic = document.querySelector('.bio__pic');
 bioPic.style.width = '26rem'
